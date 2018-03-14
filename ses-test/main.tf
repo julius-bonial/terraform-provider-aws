f41bb06d8877f35f71356bb503ba2ebe07ad10a8
Julius Daniel Herrera Glomm <julius.daniel.herrera.glomm@kaufda.de>
2018-03-14 05:59:19 +0100

    add tests

--- ses-test/main.tf
+++ ses-test/main.tf
@@ -7,9 +7,9 @@ variable "domain" {
   default = "dev-stages.global"
 }
 
-resource "aws_ses_domain_identity" "foo-bar" {
-  domain = "foo-bar.global"
-}
+# resource "aws_ses_domain_identity" "foo-bar" {
+#   domain = "foo-bar.global"
+# }
 
 resource "aws_ses_domain_identity" "ses" {
   domain = "${var.domain}"
@@ -23,22 +23,28 @@ resource "aws_sns_topic" "ses-notifications2" {
   name = "ses-notifications2"
 }
 
-resource "aws_ses_identity_notification" "bounces" {
-  identity          = "${var.domain}"
-  notification_type = "Bounce"
-  topic_arn         = "${aws_sns_topic.ses-notifications2.arn}"
+resource "aws_sns_topic" "ses-notifications3" {
+  name = "ses-notifications3"
 }
 
-resource "aws_ses_identity_notification" "complaints" {
-  identity          = "${var.domain}"
-  notification_type = "Complaint"
-  #topic_arn         = "${aws_sns_topic.ses-notifications.arn}"
+resource "aws_ses_identity_notification" "ses" {
+  identity           = "${var.domain}"
+  bounce_topic       = "${aws_sns_topic.ses-notifications.arn}"
+  complaint_topic    = "${aws_sns_topic.ses-notifications2.arn}"
+  delivery_topic     = "${aws_sns_topic.ses-notifications3.arn}"
+  forwarding_enabled = false
 }
 
-resource "aws_ses_identity_notification" "deliveries" {
-  identity          = "${var.domain}"
-  notification_type = "Delivery"
-  topic_arn         = "${aws_sns_topic.ses-notifications.arn}"
-}
-
-
+# resource "aws_ses_identity_notification" "complaints" {
+#   identity          = "${var.domain}"
+#   notification_type = "Complaint"
+#   #topic_arn         = "${aws_sns_topic.ses-notifications.arn}"
+# }
+# 
+# resource "aws_ses_identity_notification" "deliveries" {
+#   identity          = "${var.domain}"
+#   notification_type = "Delivery"
+#   topic_arn         = "${aws_sns_topic.ses-notifications.arn}"
+# }
+# 
+# 
